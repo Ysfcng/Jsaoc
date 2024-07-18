87f39e374fa2fa4a9f032afb20f12578c46193b9
Ysfcng <yc166959@gmail.com>
2024-07-18 15:29:41 -0400

    js aoc 14 gun 2 kısım bitti

--- advent/2015/14/part2/app.js
+++ advent/2015/14/part2/app.js
@@ -1,23 +1,46 @@
 const fs=require("fs")
-let text=fs.readFileSync("puz.txt",{encoding:"UTF-8"}).split("\n").filter(x=>x!="")
-let y=console.log
+let text=fs.readFileSync("puz1.txt",{encoding:"UTF-8"}).split("\n").filter(x=>x!="")
 
-let reg=/(\S+)\s\S+(\d+)\D+(\d+)\D+(\d+)/
-let  ob={}
+let ob={},duration=2503
 
-for(let i=0;i<=i<duration;i++){
 
+
+for(let i=0;i<=duration;i++){
+let arr=[]
 text.map(x=>{
+let matched=x.match(/(\S+)\D+(\d+)\D+(\d+)\D+(\d+)/)
 
+let time=+matched[3]
+let name=matched[1]
+let speed=+matched[2]
+let rest=+matched[4]
 
+if(ob[name]==undefined)
+ob[name]={}
+if(ob[name]["road"]==undefined)
+ob[name]["road"]=0
+if(ob[name]["points"]==undefined)
+ob[name]["points"]=0
 
-
-})
-
-
-
-
+if(i%(time+rest)<time){
+arr.push([name,ob[name]["road"]+speed])
+ob[name]["road"]+=speed
+}
+else{
+arr.push([name,ob[name]["road"]])
 }
 
+})
+arr=arr.sort((j,k)=>{
+return j[1] - k[1] 
+}).reverse()
+let max=arr[0][1]
 
-y(text)
+arr.map(m=>{
+if(m[1]>=max)
+ob[m[0]]["points"]+=1
+})
+console.log(arr,i)
+
+}
+console.log(ob)
